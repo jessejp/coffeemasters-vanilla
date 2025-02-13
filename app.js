9b93545bd9b52d09bb1b1a1fff417ff4df0e8823
jessejp <jessepernila@gmail.com>
2025-02-13 19:15:15 +0200

    setup fetching page data from es modules

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
+import { Store } from "./services/Store.js";
+import { loadData } from "./services/Menu.js";
 
-window.addEventListener("DOMContentLoaded", function () {
-    let nav = document.querySelector("nav")
+window.app = {
+    store: Store
+}
+
+window.addEventListener("DOMContentLoaded", async function () {
+    loadData();
 })
---
+++ services/API.js
@@ -0,0 +1,8 @@
+
+export const API = {
+    url: "/data/menu.json",
+    fetchMenu: async() => {
+        const result = await fetch(API.url);
+        return await result.json();
+    }
+}
